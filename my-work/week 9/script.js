fde877428010e322744aa5ae225de6b9eea74646
Aleksandra Medina <am7625@nyu.edu>
2019-11-05 15:59:47 +0800

    hw

--- my-work/week 9/script.js
+++ my-work/week 9/script.js
@@ -9,26 +9,92 @@ let viz = d3.select("#container")
     .style("height", h)
 ;
 
-function gotData(data){
+function gotData(incomingData){
 
-let questionOne = data.e1
-let questionTwo = data.e2
+  let yeses2009 = [];
+  let noes2009 = [];
 
-console.log ("Can a woman legally sign a contract in the same way as a man?");
-console.log (questionOne);
+  let yeses2018 = [];
+  let noes2018 = [];
 
-console.log ("Can a woman legally register a business in the same way as a man?");
-console.log(questionTwo);
+  let year2009 = [];
+  let year2018 = [];
 
-console.log (data);
+  console.log(incomingData);
+
+  incomingData.forEach((country) => {
+    let year = country.year
+    let questionOne = country.e1
+    let questionTwo = country.e2
+
+    if (year == '2009'){
+      year2009.push(year);
+      if (questionOne == 'Yes') {
+        yeses2009.push(questionOne);
+      } else if (questionOne == 'No') {
+        noes2009.push(questionOne);
+      }
+    } else if (year == '2018'){
+      year2018.push(year);
+      if (questionOne == 'Yes') {
+        yeses2018.push(questionOne);
+      } else if (questionOne == 'No') {
+        noes2018.push(questionOne);
+      }
+    }
+  })
 
 
-function filterYes(){
-    return (data.e1 == "Yes")
+  // incomingData.forEach((country) => {
+  //   let questionOne = country.e1
+  //   let questionTwo = country.e2
+  //
+  //   if (questionOne == 'Yes') {
+  //     yeses.push(questionOne);
+  //   } else if (questionOne == 'No') {
+  //     noes.push(questionOne);
+  //   }
+  // })
+
+  // console.log(yeses, noes);
+console.log ('in 2009 countries that scored YES on Q1:')
+console.log (yeses2009.length);
+console.log ('in 2009 countries that scored NO on Q1:')
+console.log (noes2009.length);
+console.log ('in 2018 countries that scored YES on Q1:')
+console.log (yeses2018.length);
+console.log ('in 2018 countries that scored NO on Q1:')
+console.log (noes2018.length);
+
+
+console.log(year2009.length);
+console.log(year2018.length);
+
+
+
+
+let datagroups1 = viz.selectAll(".datagroup1").data(yeses2009).enter()
+  .append("g")
+    .classed("datagroup1", true)
+
+let datagroups2 = viz.selectAll(".datagroup2").data(noes2009).enter()
+  .append("g")
+    .classed("datagroup2", true)
+
+let scoresYes2009 = datagroups1.append("rect")
+    .attr("x", function(d,i){return 10*i})
+    .attr("y", 200)
+    .attr("width", 5)
+    .attr("height", 200)
+    .attr("fill", 'green')
+
+let scoresNo2009 = datagroups2.append("rect")
+    .attr("x", function(d,i){return 10*i})
+    .attr("y", 400)
+    .attr("width", 5)
+    .attr("height", 200)
+    .attr("fill", 'red')
+
 }
 
-function filterNo(){
-    return (data.e1 == "No")
-}
-console.log (filterYes)
-}
+d3.json("data.json").then(gotData);
